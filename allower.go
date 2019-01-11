b2365c3e03cfa07fd126bb9a7f40a78b65d31976
Christine Dodrill <me@christine.website>
2019-01-11 15:55:10 -0800

    MapConfig: simple wrapper around a string->slice of string map

--- allower.go
+++ allower.go
@@ -3,7 +3,9 @@ package confyg
 // Allower defines if a given verb and block combination is valid for
 // configuration parsing.
 //
-// Return false if this verb and block pair is invalid.
+// If this is intended to be a statement-like verb, block should be set
+// to false. If this is intended to be a block-like verb, block should
+// be set to true.
 type Allower interface {
 	Allow(verb string, block bool) bool
 }
--- reader.go
+++ reader.go
@@ -4,6 +4,9 @@ import "bytes"
 
 // Reader is called when individual lines of the configuration file are being read.
 // This is where you should populate any relevant structures with information.
+//
+// If something goes wrong in the file parsing step, add data to the errs buffer
+// describing what went wrong.
 type Reader interface {
 	Read(errs *bytes.Buffer, fs *FileSyntax, line *Line, verb string, args []string)
 }
