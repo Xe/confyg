39eaf7bce89f8f80a7a423b46bac7cfdebc871d7
Christine Dodrill <me@christine.website>
2019-01-11 16:14:12 -0800

    flagconfyg: a wrapper for programs

---
+++ flagconfyg/flagconfyg.go
@@ -0,0 +1,44 @@
+// Package flagconfyg is a hack around confyg. This will blindly convert config
+// verbs to flag values.
+package flagconfyg
+
+import (
+	"bytes"
+	"flag"
+	"io/ioutil"
+	"log"
+	"strings"
+
+	"within.website/confyg"
+)
+
+// CmdParse is a quick wrapper for command usage. It explodes on errors.
+func CmdParse(path string) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = Parse(path, data, flag.CommandLine)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Parse parses the config file in the given file by name, bytes data and into
+// the given flagset.
+func Parse(name string, data []byte, fs *flag.FlagSet) error {
+	lineRead := func(errs *bytes.Buffer, fs_ *confyg.FileSyntax, line *confyg.Line, verb string, args []string) {
+		err := fs.Set(verb, strings.Join(args, " "))
+		if err != nil {
+			errs.WriteString(err.Error())
+		}
+	}
+
+	_, err := confyg.Parse(name, data, confyg.ReaderFunc(lineRead), confyg.AllowerFunc(allower))
+	return err
+}
+
+func allower(verb string, block bool) bool {
+	return true
+}
---
+++ flagconfyg/flagconfyg_test.go
@@ -0,0 +1,31 @@
+package flagconfyg
+
+import (
+	"flag"
+	"testing"
+)
+
+func TestFlagConfyg(t *testing.T) {
+	fs := flag.NewFlagSet("test", flag.PanicOnError)
+	sc := fs.String("subscribe", "", "to pewdiepie")
+	us := fs.String("unsubscribe", "", "from t-series")
+
+	const configFile = `subscribe pewdiepie
+
+unsubscribe (
+  t-series
+)`
+
+	err := Parse("test.cfg", []byte(configFile), fs)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if *sc != "pewdiepie" {
+		t.Errorf("wanted subscribe->pewdiepie, got: %s", *sc)
+	}
+
+	if *us != "t-series" {
+		t.Errorf("wanted unsubscribe->t-series, got: %s", *us)
+	}
+}
